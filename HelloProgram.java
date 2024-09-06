9bc86150299b08a270e26781eba6f50606e560c1
Sharanya-SDET <sharanyak119@gmail.com>
2024-09-05 22:23:43 -0400

    Add files via upload

---
+++ HelloProgram.java
@@ -0,0 +1,19 @@
+package seleniumPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloProgram {
+	public static void main(String[]args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.numpyninja.com/contact");
+		
+		
+		
+		
+	}
+	
+	
+}
